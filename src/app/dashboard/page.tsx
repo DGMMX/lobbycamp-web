f9afc25307fae655335af4238b872108a815a06e
DGMMX <dgmmxs2@outlook.com>
2025-04-02 23:28:15 -0300

    imagem colocada e add readme

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ export default function DashboardPage() {
         <>
         <NavBar active="dashboard"/>
         <main className="flex  justify-center items-center">
-            <div className="bg-yellow-500 min-w-2/3 m-6 p-6 rounded">
+            <div className="bg-yellow-500 81% min-w-2/3 m-6 p-6 rounded ">
                 <h2 className="text-lg font-bold">Dashpord</h2>
             </div>
         </main>
--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -7,7 +7,7 @@ export default function TransactionsPage() {
             <NavBar active="movimentações" />
 
             <main className="flex justify-center">
-                <div className="bg-slate-900 min-w-2/3 m-6 p-6 rounded">
+                <div className="bg-yellow-500 min-w-2/3 m-6 p-6 rounded">
                     <h2 className="text-lg font-bold">Movimentações</h2>
                 </div>
             </main>
